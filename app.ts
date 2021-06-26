206c0a2b99a9e89bb8360167e4dd1f5f15ae6e96
tks18 <tksudharshan@gmail.com>
2021-06-26 15:14:29 +0530

    Add Basic Modules, Cors Headers and Other Sanitization Functions

--- app.ts
+++ app.ts
@@ -1,6 +1,22 @@
+// Initialization
+import dotenv from 'dotenv';
 import express from 'express';
+import helmet from 'helmet';
+import mongoSanitize from 'express-mongo-sanitize';
+import xssProtect from 'x-xss-protection';
 
+// Middlewares
+import origin from './middlewares/check-origin';
+
+dotenv.config();
+
+// Express config
 const app = express();
+app.use(express.json({ limit: '50kb' }));
+app.use(helmet());
+app.use(xssProtect());
+app.use(mongoSanitize());
+app.use(origin);
 
 app.get('/', (req, res) => {
   res.send('Init the Server');
