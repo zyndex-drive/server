f5cdbe07ecb8127a91803e42eca23f91c3f4237d
tks18 <tksudharshan@gmail.com>
2021-06-27 11:53:20 +0530

    Eslint: Add plugins and Necessary Stuffs for Linting Cors: Fix Cors Linting Errors

--- app.ts
+++ app.ts
@@ -6,8 +6,9 @@ import mongoSanitize from 'express-mongo-sanitize';
 import xssProtect from 'x-xss-protection';
 
 // Middlewares
-import origin from './middlewares/check-origin';
+import cors from './middlewares/cors';
 
+// Load ENV Variables to the Process
 dotenv.config();
 
 // Express config
@@ -16,7 +17,7 @@ app.use(express.json({ limit: '50kb' }));
 app.use(helmet());
 app.use(xssProtect());
 app.use(mongoSanitize());
-app.use(origin);
+app.use(cors);
 
 app.get('/', (req, res) => {
   res.send('Init the Server');
