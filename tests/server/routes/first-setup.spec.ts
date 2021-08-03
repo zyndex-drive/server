8698bf9ad9c1924c5c7d0c4011d9dc1ea7bf3840
tks18 <tksudharshan@gmail.com>
2021-08-03 19:43:21 +0530

    test(first-setup): write tests for policies, roles, credentials route tests for first-setup

--- tests/server/routes/first-setup.spec.ts
+++ tests/server/routes/first-setup.spec.ts
@@ -2,12 +2,84 @@ import { step } from 'mocha-steps';
 import { postRequester } from '../../test-helpers/requester';
 
 const { URL } = process.env;
+const SAMPLE_CREDENTIALS = {
+  alias: 'test_id',
+  client_id: 'testing@sssuper.com',
+  client_secret: 'sadasdas8d0ad0asasd',
+  email: 'testing@supercool.com',
+};
 
 describe('Testing First Setup Route', () => {
   step('Get all Routes Under Setup Route', (done) => {
     postRequester(URL, '/setup/endpoints', {}, (err, response) => {
       if (!err && response.status === 200) {
-        response.body.data.should.be.a('object');
+        response.body.should.be.a('object');
+        done();
+      } else {
+        done(err);
+      }
+    });
+  });
+  step('Add Default Policies to Database', (done) => {
+    postRequester(URL, '/setup/policies/add', {}, (err, response) => {
+      if (!err && response.status === 200) {
+        response.body.should.be.a('object');
+        response.body.should.have.a.property('status').to.be.eql(200);
+        done();
+      } else {
+        done(err);
+      }
+    });
+  });
+  step('Check Status of Policies Added Before', (done) => {
+    postRequester(URL, '/setup/policies/status', {}, (err, response) => {
+      if (!err && response.status === 200) {
+        response.body.should.be.a('object');
+        response.body.should.have.a.property('status').to.be.eql(200);
+        done();
+      } else {
+        done(err);
+      }
+    });
+  });
+  step('Reset Default Policies added before', (done) => {
+    postRequester(URL, '/setup/policies/reset', {}, (err, response) => {
+      if (!err && response.status === 200) {
+        response.body.should.be.a('object');
+        response.body.should.have.a.property('status').to.be.eql(200);
+        done();
+      } else {
+        done(err);
+      }
+    });
+  });
+  step('Add Default Roles to Database', (done) => {
+    postRequester(URL, '/setup/roles/add', {}, (err, response) => {
+      if (!err && response.status === 200) {
+        response.body.should.be.a('object');
+        response.body.should.have.a.property('status').to.be.eql(200);
+        done();
+      } else {
+        done(err);
+      }
+    });
+  });
+  step('Check Status of Roles Added before', (done) => {
+    postRequester(URL, '/setup/roles/status', {}, (err, response) => {
+      if (!err && response.status === 200) {
+        response.body.should.be.a('object');
+        response.body.should.have.a.property('status').to.be.eql(200);
+        done();
+      } else {
+        done(err);
+      }
+    });
+  });
+  step('Reset Default Roles added before', (done) => {
+    postRequester(URL, '/setup/roles/reset', {}, (err, response) => {
+      if (!err && response.status === 200) {
+        response.body.should.be.a('object');
+        response.body.should.have.a.property('status').to.be.eql(200);
         done();
       } else {
         done(err);
@@ -15,22 +87,14 @@ describe('Testing First Setup Route', () => {
     });
   });
   step('Add Sample Credentials to Database', (done) => {
-    const sampleCreds = {
-      alias: 'test_id',
-      client_id: 'testing@sssuper.com',
-      client_secret: 'sadasdas8d0ad0asasd',
-      email: 'testing@supercool.com',
-    };
     postRequester(
       URL,
       '/setup/credentials/add',
-      sampleCreds,
+      SAMPLE_CREDENTIALS,
       (err, response) => {
         if (!err && response.status === 200) {
-          console.log(response);
-          response.body.data.should.be.a('object');
-          response.status.should.be.eql(200);
-          response.body.data.should.have.a.property('status').to.be.eql(200);
+          response.body.should.be.a('object');
+          response.body.should.have.a.property('status').to.be.eql(200);
           done();
         } else {
           done(err);
@@ -41,9 +105,8 @@ describe('Testing First Setup Route', () => {
   step('Reset Test Credentials added before', (done) => {
     postRequester(URL, '/setup/credentials/reset', {}, (err, response) => {
       if (!err && response.status === 200) {
-        response.body.data.should.be.a('object');
-        response.status.should.be.eql(200);
-        response.body.data.should.have.a.property('status').to.be.eql(200);
+        response.body.should.be.a('object');
+        response.body.should.have.a.property('status').to.be.eql(200);
         done();
       } else {
         done(err);
