fdfa1764fb448729b5e23d8fb247c467bf00a010
tks18 <tksudharshan@gmail.com>
2021-08-06 21:17:30 +0530

    feat(models/tokens): introduce tokens collection for managing auth tokens

--- models/index.ts
+++ models/index.ts
@@ -5,6 +5,7 @@ export { default as Scopes } from './scope';
 export { default as PendingUsers } from './pending-user';
 export { default as BlacklistUsers } from './blacklisted-user';
 export { default as Credentials } from './credential';
+export { default as Tokens } from './tokens';
 export { default as Roles } from './role';
 export { default as Policies } from './policy';
 export { default as Frontends } from './frontend';
---
+++ models/tokens/statics.ts
@@ -0,0 +1,27 @@
+import createDocument from '@/helpers/db/models/static/create-document';
+import clearCollection from '@/helpers/db/models/static/clear-collection';
+
+// Types
+import { IToken, ITokenDoc, ITokenModel } from './types';
+import type { IInlineResponse } from '@typs/inline.response';
+
+/**
+ * Create a SMTP Provider Document and Save it to Database
+ *
+ * @param {ITokenModel} this - SMTP Provider Model
+ * @param {IToken} doc - SMTP Provider to be Created and Saved
+ * @returns {Promise<ITokenDoc>} - Promise Returning Saved Document
+ */
+export function createDoc(this: ITokenModel, doc: IToken): Promise<ITokenDoc> {
+  return createDocument<IToken, ITokenDoc, ITokenModel>(this, doc);
+}
+
+/**
+ * Clears the SMTP Provider Collection by Deleting all the Records
+ *
+ * @param {ITokenModel} this - SMTP Provider Model
+ * @returns {Promise<IInlineResponse<string>>} - Response whether cleared or not
+ */
+export function clearAll(this: ITokenModel): Promise<IInlineResponse<string>> {
+  return clearCollection<ITokenDoc, ITokenModel>(this);
+}
