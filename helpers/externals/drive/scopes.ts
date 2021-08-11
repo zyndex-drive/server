e8a1b7455945f4937d1134010b5a3cc9a744f435
tks18 <tksudharshan@gmail.com>
2021-08-11 10:16:48 +0530

    feat(google-oauth): add google oauth generator handler for express

---
+++ helpers/externals/drive/scopes.ts
@@ -0,0 +1,10 @@
+import type { TGooGScope } from '@externals/google/types';
+
+const scopes: TGooGScope[] = [
+  'https://www.googleapis.com/auth/drive',
+  'https://www.googleapis.com/auth/drive.file',
+  'https://www.googleapis.com/auth/drive.metadata',
+  'https://www.googleapis.com/auth/drive.file',
+];
+
+export default scopes;
---
+++ helpers/externals/google/index.ts
@@ -0,0 +1,1 @@
+export { default as endpoints } from './endpoints';
