8ac4f78589e61b9b0d24c43232bfc8120be9eff6
tks18 <tksudharshan@gmail.com>
2021-08-15 11:44:31 +0530

    feat(google): write types for google oauth functions

--- helpers/externals/google/types.d.ts
+++ helpers/externals/google/types.d.ts
@@ -1,1 +1,8 @@
 export type TGooGScope = `https://www.googleapis.com/auth/${string}`
+
+export interface IGoogTokenResponse {
+    access_token: string;
+    expires_in: number;
+    refresh_token?: string;
+    token_type: string;
+}
