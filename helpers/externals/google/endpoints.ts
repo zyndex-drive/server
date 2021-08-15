5d63ea848745442aea52320ef33da93efcb7a18d
tks18 <tksudharshan@gmail.com>
2021-08-15 11:41:07 +0530

    refactor(google/endpoints): remove google endpoints file

--- helpers/externals/google/endpoints.ts
+++
@@ -1,5 +0,0 @@
-export default {
-  authGen: 'https://accounts.google.com/o/oauth2/v2/auth',
-  authServer: 'https://oauth2.googleapis.com/token',
-  authCheck: 'https://oauth2.googleapis.com/tokeninfo',
-};
