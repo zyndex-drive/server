5d0ae5bcb0b9832d6bc2cc05385f38a6ea4ad871
tks18 <tksudharshan@gmail.com>
2021-10-01 18:08:12 +0530

    feat(google/service-account): handler for saving checking and saving service account token

--- google/index.ts
+++ google/index.ts
@@ -4,6 +4,7 @@ export { api as oauthAuthenticationApis } from './helpers';
 export { api as oauthRouteApis } from './API';
 
 export { default as normalAccountHandler } from './handlers/nac';
+export { default as serviceAccountHandler } from './handlers/sac';
 
 export const oauthHelpers = {
   resolveToken,
