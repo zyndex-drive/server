5402fd11719d59205682d26c0c4b9bf71808e3dd
tks18 <tksudharshan@gmail.com>
2021-10-09 15:17:44 +0530

    feat(google/helpers): add function to construct google fields parameter

--- google/helpers/index.ts
+++ google/helpers/index.ts
@@ -3,3 +3,4 @@ export { default as resolveToken } from './resolve-token';
 export { default as stringizeScope } from './stringize-scope';
 export { default as googleApiRequest } from './requester';
 export { default as createJwtToken } from './sign-jwt';
+export { default as constructFields } from './construct-fields';
