254dad7f5e8846c4bdcb0ca40c46e7ca0b7fdd79
Sudharshan TK <tksudharshan@gmail.com>
2021-11-25 07:47:39 +0530

    feat(db/plugins): write a hash mongoose plugin
    hash plugin helps in hashing fields such as passwords using bcrypt while saving it to the database

---
+++ plugins/db/plugins/hash/index.ts
@@ -0,0 +1,28 @@
+import { fieldsPicker, hashString } from '@plugins/db/helpers';
+
+import type { Document, Model, Schema, HookNextFunction } from 'mongoose';
+
+/**
+ * Creates a Hash Plugin which will add option to hash Fields using Bcrypt in a Schema
+ *
+ * @returns {Function} Hash Plugin
+ */
+export default function <T, U extends Document, V extends Model<U>>(): (
+  schema: Schema<U, V, T>,
+) => void {
+  const plugin = (schema: Schema<U, V, T>): void => {
+    const toHashFields: string[] = fieldsPicker<T, U, V>(schema, 'hash');
+    schema.pre('validate', function (this: U, next: HookNextFunction) {
+      hashString<U>(this, toHashFields)
+        .then((hashedDoc) => {
+          this.set(hashedDoc);
+          next();
+        })
+        .catch((err) => {
+          console.log(err);
+          throw new Error('Password Hashing Failed');
+        });
+    });
+  };
+  return plugin;
+}
