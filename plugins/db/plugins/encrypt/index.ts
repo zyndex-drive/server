b3292fb5971822975aaf119763dd13d6284b9dad
Sudharshan TK <tksudharshan@gmail.com>
2021-11-25 07:46:32 +0530

    feat(db/plugins): write a crypto mongoose plugin
    crypto plugin helps in encrypting and decrypting data to and from the database

---
+++ plugins/db/plugins/encrypt/index.ts
@@ -0,0 +1,30 @@
+import {
+  fieldsPicker,
+  encryptFields,
+  decryptFields,
+} from '@plugins/db/helpers';
+
+import type { Document, Model, Schema, HookNextFunction } from 'mongoose';
+
+/**
+ * Creates a Crypto Plugin which will add option to encrypt Fields in a Schema
+ *
+ * @returns {Function} Crypto Plugin
+ */
+export default function <T, U extends Document, V extends Model<U>>(): (
+  schema: Schema<U, V, T>,
+) => void {
+  const plugin = (schema: Schema<U, V, T>): void => {
+    const encryptedFields: string[] = fieldsPicker<T, U, V>(schema, 'encrypt');
+    schema.pre('validate', function (this: U, next: HookNextFunction) {
+      const encryptedDoc = encryptFields(this, encryptedFields);
+      this.set(encryptedDoc);
+      next();
+    });
+    schema.post('init', function (this: U) {
+      const decryptedDoc = decryptFields<U>(this, encryptedFields);
+      return decryptedDoc;
+    });
+  };
+  return plugin;
+}
