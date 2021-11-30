9c993dd465fe45d917724a5be3162a9c81f3ef59
Sudharshan TK <tksudharshan@gmail.com>
2021-11-30 15:27:41 +0530

    ci(build): add support for minification for final code

--- scripts/prepare-build.sh
+++ scripts/prepare-build.sh
@@ -11,6 +11,9 @@ cd ..
 echo "Building Typescript Files"
 yarn build:tsc
 
+echo "Obfuscating Code"
+bash ./scripts/obfuscate.sh
+
 echo "Doing Some Miscellaneous things to make build ready"
 cd dist
 mkdir .yarn
@@ -21,6 +24,7 @@ cp ../.yarnrc.yml ./
 cp ../README.md ./
 cp ../LICENSE ./
 cp ../.gitignore ./
+cp ../CHANGELOG.md ./
 cp -r ../.yarn/plugins ./.yarn/
 cp -r ../.yarn/releases ./.yarn/
 
@@ -28,4 +32,8 @@ echo "Pushing into Repo"
 git add .
 git commit -m "$1"
 git push --follow-tags origin build
+
+echo "Cleaning Directory"
+cd ..
+rm -rf ./out
 
