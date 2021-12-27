5f4716cbcaf3a789b4d220c432d81827219a5f40
Sudharshan TK <tksudharshan@gmail.com>
2021-12-27 20:30:03 +0530

    feat(views): add backend dashboard html files

---
+++ views/precache-manifest.d51489f5c5b1b2c7633da1d965dbf182.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cd36c0262ab0b236178",
+    "url": "/css/chunk-vendors.392be852.css"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "80583274873bc7ed1e60f702298164be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65136f368ed808b26747",
+    "url": "/js/about.abf9e668.js"
+  },
+  {
+    "revision": "cc2d05ff3d81ea5f4cf9",
+    "url": "/js/app.150b19d6.js"
+  },
+  {
+    "revision": "8cd36c0262ab0b236178",
+    "url": "/js/chunk-vendors.6f028e18.js"
+  },
+  {
+    "revision": "af3442210705525bcba5cbbc7e4307bf",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
