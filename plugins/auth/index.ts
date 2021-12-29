4eda2b4f328fee7a60600f8fdcd16cbcfc82c646
Sudharshan TK <tksudharshan@gmail.com>
2021-12-29 08:29:19 +0530

    chore: add some misc index.ts files for easy imports

---
+++ plugins/auth/index.ts
@@ -0,0 +1,7 @@
+import user from './user';
+
+export * from './helpers';
+export * from './user';
+export default {
+  user,
+};
---
+++ plugins/auth/user/index.ts
@@ -0,0 +1,43 @@
+import {
+  viewer as accViewer,
+  contentMgr as accCntMgr,
+  manager as accMgr,
+  moderator as accMods,
+} from './accept';
+import {
+  viewer as blackViewer,
+  contentMgr as blackCntMgr,
+  manager as blackMgr,
+  moderator as blackMods,
+} from './blacklist';
+import {
+  contentMgr as promCntMgr,
+  manager as promMgr,
+  moderator as promMods,
+} from './promote';
+
+export const accept = {
+  viewer: accViewer,
+  contentMgr: accCntMgr,
+  moderator: accMods,
+  manager: accMgr,
+};
+
+export const blacklist = {
+  viewer: blackViewer,
+  contentMgr: blackCntMgr,
+  moderator: blackMods,
+  manager: blackMgr,
+};
+
+export const promote = {
+  contentMgr: promCntMgr,
+  moderator: promMods,
+  manager: promMgr,
+};
+
+export default {
+  accept,
+  blacklist,
+  promote,
+};
