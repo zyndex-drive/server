df311c80518c0c6734144a5ec46752d4e919d6ef
Sudharshan TK <tksudharshan@gmail.com>
2022-02-04 19:22:21 +0530

    feat(plugins/jwt): use jose to generate rsa and es256 keys

--- plugins/json-web-token/generate-keys.ts
+++ plugins/json-web-token/generate-keys.ts
@@ -1,36 +1,40 @@
-import { promisify } from 'util';
-import { generateKeyPair } from 'crypto';
+import { Keys } from '@models';
+import { objectID } from '@plugins/misc';
+import { generateKeyPair, generateSecret, exportJWK } from 'jose';
 
-const promisedgenerateKeyPair = promisify(generateKeyPair);
-
-interface IKeyPairs {
-  privateKey: string;
-  publicKey: string;
-}
+import type { IKeyDoc } from '@models/types';
 
 /**
  * Generates a PEM based Public and Private Keys
  *
- * @returns {Promise<IKeyPairs>} - Promise Resolving to Key pairs
+ * @returns {Promise<IKeyDoc[]>} - Promise Resolving to Key pairs
  */
-export default async function (): Promise<IKeyPairs> {
-  const { GLOBAL_PASSPHRASE } = process.env;
-    if (GLOBAL_PASSPHRASE) {
-      const keyPairs = await promisedgenerateKeyPair('rsa', {
-        modulusLength: 2048,
-        publicKeyEncoding: {
-          type: 'spki',
-          format: 'pem',
-        },
-        privateKeyEncoding: {
-          type: 'pkcs8',
-          format: 'pem',
-          cipher: 'aes-256-cbc',
-          passphrase: GLOBAL_PASSPHRASE,
-        },
-      })
-      return keyPairs;
-    } else {
-      throw new Error('No Global Passphrase is Found in the Environment Variables')
-    }
+export default async function (): Promise<IKeyDoc[]> {
+  const { privateKey, publicKey } = await generateKeyPair('PS256');
+  const secretKey = await generateSecret('HS256');
+  const [pvtKey, pubKey, sctKey] = await Promise.all([
+    exportJWK(privateKey),
+    exportJWK(publicKey),
+    exportJWK(secretKey),
+  ]);
+  const [uid1, uid2, uid3] = [objectID(), objectID(), objectID()];
+  const newkeys = [
+    {
+      _id: uid1,
+      type: 'privatekey',
+      key: pvtKey,
+    },
+    {
+      _id: uid2,
+      type: 'publickey',
+      key: pubKey,
+    },
+    {
+      _id: uid3,
+      type: 'secretkey',
+      key: sctKey,
+    },
+  ];
+  const savedKeys = await Keys.create(newkeys);
+  return savedKeys;
 }
