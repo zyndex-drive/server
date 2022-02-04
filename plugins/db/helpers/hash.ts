824975eee756ff3320cc9fac39a90d71929b3bc9
Sudharshan TK <tksudharshan@gmail.com>
2022-02-04 19:16:15 +0530

    refactor(plugins/db/helpers): refactor db hash plugin from promises to async/await

--- plugins/db/helpers/hash.ts
+++ plugins/db/helpers/hash.ts
@@ -8,39 +8,33 @@ import dotProp from 'dot-prop';
  * @param {string[]} encryptedFields - Array of Fields to Hash
  * @returns {Object} Object with Hashed Fields
  */
-export default function <T>(obj: T, encryptedFields?: string[]): Promise<T> {
-  return new Promise<T>((resolve, reject) => {
-    try {
-      if (encryptedFields) {
-        const forLoopPromise = new Promise<T>((resolve, reject) => {
-          let modObj = obj;
-          encryptedFields.forEach((field: string, index: number) => {
-            if (dotProp.has(obj, field)) {
-              const inValue = dotProp.get(obj, field);
-              if (typeof inValue === 'string') {
-                bcrypt
-                  .hash(inValue, 10)
-                  .then((result) => {
-                    modObj = dotProp.set(modObj, field, result);
-                  })
-                  .catch((err: string) => {
-                    reject(err);
-                  });
-              }
+export default async function <T>(
+  obj: T,
+  encryptedFields?: string[],
+): Promise<T> {
+  let modObj = obj;
+  if (encryptedFields) {
+    const promises = encryptedFields.map(
+      (field: string) =>
+        new Promise<void>((resolve, reject) => {
+          if (dotProp.has(obj, field)) {
+            const inValue = dotProp.get(obj, field);
+            if (typeof inValue === 'string') {
+              bcrypt
+                .hash(inValue, 10)
+                .then((result) => {
+                  modObj = dotProp.set(obj, field, result);
+                  resolve();
+                })
+                .catch((err: string) => {
+                  reject(err);
+                });
             }
-            if (index === encryptedFields.length - 1) resolve(modObj);
-          });
-        });
-        forLoopPromise
-          .then((modObj) => resolve(modObj))
-          .catch((err) => {
-            console.log(err);
-            reject(err);
-          });
-      }
-    } catch (err) {
-      console.log(err);
-      reject(err);
-    }
-  });
+          }
+        }),
+    );
+    await Promise.all(promises);
+    return modObj;
+  }
+  return modObj;
 }
