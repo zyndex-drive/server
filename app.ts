55fcb37afbfeb4bb0860f2bb69e48d27a3659fa0
Sudharshan TK <tksudharshan@gmail.com>
2022-02-09 19:36:14 +0530

    feat(app.ts): add passport initialize function after db connects

--- app.ts
+++ app.ts
@@ -2,7 +2,11 @@
 import dotenv from 'dotenv';
 
 // Import Server and Database
+import os from 'os';
 import { server, db } from '@plugins';
+
+// Passport Imports
+import { initializePassport } from '@plugins/oauth';
 
 // Health Check Service
 import { healthCheckService } from '@plugins/server/helpers';
@@ -17,14 +21,17 @@ healthCheckService(server);
 const PORT = process.env.PORT || 3000;
 try {
   server.listen(PORT, () => {
+    console.log(`Environment: ${os.type()}`);
     console.log(`Server Started on Port: ${PORT}`);
     console.log('Connecting to Database.....');
 
-    // Connect to Datbase
+    // Connect to Database
     db.connect()
       .then(() => {
         console.log('Database Connected...OK..');
       })
+      .then(() => initializePassport())
+      .then(() => console.log('Passport Oauth Clients Initialized'))
       .catch((err: string) => {
         console.log(err);
         server.close();
