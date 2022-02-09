386c928665d187b64775dd8af2d9bc57e5fdde43
Sudharshan TK <tksudharshan@gmail.com>
2022-02-09 19:23:25 +0530

    fix(plugins/sessionmanager): move more redundant tasks to the main create session function

--- plugins/session-manager/create-session.ts
+++ plugins/session-manager/create-session.ts
@@ -1,74 +1,105 @@
 import { objectID } from '@plugins/misc';
-import { Sessions, GlobalSettings } from '@models';
+import { Sessions, GlobalSettings, Frontends } from '@models';
 import { generateJWT } from '@plugins/json-web-token';
+import { NotFound } from '@plugins/errors';
 
 import type { Request } from 'express';
-import type {
-  ISessionDoc,
-  IFrontendLeanDoc,
-  IUserLeanDoc,
-} from '@models/types';
+import type { ISessionDoc, IUserDoc } from '@models/types';
 import type { JWTPayload } from 'jose';
+
+interface ISessionResponse {
+  _id: string;
+  frontend: string;
+  ip: string;
+  issued_at: number;
+  token_secret: string;
+  user_id: string;
+  expires_at: number;
+  roles: { role: string; scope: string }[];
+}
+
+const generateResponse = (
+  sessionDoc: ISessionDoc,
+  userDoc: IUserDoc,
+): ISessionResponse => {
+  const sessionResponse = {
+    _id: String(sessionDoc._id),
+    frontend: String(sessionDoc.frontend),
+    ip: sessionDoc.ip,
+    issued_at: sessionDoc.issued_at,
+    token_secret: sessionDoc.token_secret,
+    user_id: String(sessionDoc.user_id),
+    expires_at: sessionDoc.expires_at,
+    roles: [
+      ...userDoc.roles.map((role) => ({
+        role: String(role.role),
+        scope: String(role.scope),
+      })),
+    ],
+  };
+  return sessionResponse;
+};
 
 /**
  * Generates a Session for a user for a Particular Frontend
  *
  * @param {Request} req - Request Express Object
- * @param {IUserLeanDoc} user - User Document from the Database
- * @param {IFrontendLeanDoc} frontend - Frontend Document from the Database
- * @returns {Promise<ISessionDoc>} - Promise Resolving to Session Document
+ * @param {IUserDoc} user - User Document from the Database
+ * @param {string} frontend - Frontend Document from the Database
+ * @returns {Promise<ISessionResponse>} - Promise Resolving to Session Document
  */
 export default async function (
   req: Request,
-  user: IUserLeanDoc,
-  frontend: IFrontendLeanDoc,
-): Promise<ISessionDoc> {
+  user: IUserDoc,
+  frontend: string,
+): Promise<ISessionResponse> {
+  const frontendDoc = await Frontends.findOne({ _id: frontend })
+    .lean()
+    .orFail(() => new NotFound('Frontend ID Not Found in the Database'))
+    .exec();
   const maxSessions = await GlobalSettings.findOne({ code: 'max-sessions' })
     .lean()
+    .orFail(() => new NotFound('Max Sessions Flag not Found in the Database'))
     .exec();
-  if (maxSessions) {
-    const totalSessions = maxSessions.global_flag;
-    const payload: JWTPayload = {
-      ip: req.ip,
-      user_id: String(user._id),
-      frontend: String(frontend._id),
-    };
-    const sessionDocs = await Sessions.find({ user_id: user._id })
-      .lean()
-      .exec();
-    if (sessionDocs.length >= totalSessions) {
-      await Sessions.deleteOne({ user_id: user._id });
-      const token = await generateJWT(user, payload);
-      const newId = objectID();
-      const now = Date.now();
-      const expiryPeriod = 30 * 60 * 60 * 1000;
-      const expiry = now + expiryPeriod;
-      const newSession = new Sessions({
-        _id: newId,
-        ...payload,
-        token_secret: token,
-        issued_at: now,
-        expires_at: expiry,
-      });
-      const sessionDoc = await newSession.save();
-      return sessionDoc;
-    } else {
-      const token = await generateJWT(user, payload);
-      const newId = objectID();
-      const now = Date.now();
-      const expiryPeriod = 30 * 60 * 60 * 1000;
-      const expiry = now + expiryPeriod;
-      const newSession = new Sessions({
-        _id: newId,
-        ...payload,
-        token_secret: token,
-        issued_at: now,
-        expires_at: expiry,
-      });
-      const sessionDoc = await newSession.save();
-      return sessionDoc;
-    }
+  const totalSessions = maxSessions.global_flag;
+  const payload: JWTPayload = {
+    ip: req.ip,
+    user_id: String(user._id),
+    frontend: String(frontendDoc._id),
+  };
+  const sessionDocs = await Sessions.find({ user_id: user._id }).lean().exec();
+  if (sessionDocs.length >= totalSessions) {
+    await Sessions.deleteOne({ user_id: user._id });
+    const token = await generateJWT(user, payload);
+    const newId = objectID();
+    const now = Date.now();
+    const expiryPeriod = 30 * 60 * 60 * 1000;
+    const expiry = now + expiryPeriod;
+    const newSession = new Sessions({
+      _id: newId,
+      ...payload,
+      token_secret: token,
+      issued_at: now,
+      expires_at: expiry,
+    });
+    const sessionDoc = await newSession.save();
+    const response = generateResponse(sessionDoc, user);
+    return response;
   } else {
-    throw new Error('Max Sessions Flag not Found in the Database');
+    const token = await generateJWT(user, payload);
+    const newId = objectID();
+    const now = Date.now();
+    const expiryPeriod = 30 * 60 * 60 * 1000;
+    const expiry = now + expiryPeriod;
+    const newSession = new Sessions({
+      _id: newId,
+      ...payload,
+      token_secret: token,
+      issued_at: now,
+      expires_at: expiry,
+    });
+    const sessionDoc = await newSession.save();
+    const response = generateResponse(sessionDoc, user);
+    return response;
   }
 }
