e20429492f43ebb92d6c51439b822a0463d83881
tks18 <tksudharshan@gmail.com>
2022-06-25 16:22:21 +0530

    feat(routes/auth/policies): add new route for editing policy model with the new express handler

--- src/routes/auth/policies/index.ts
+++ src/routes/auth/policies/index.ts
@@ -9,11 +9,18 @@ import { policies as policyAuth } from '@plugins/auth';
 
 // Response Handlers
 import { errorResponseHandler, okResponse } from '@plugins/server/responses';
+import { editDatabaseExpressHandler } from '@plugins/server/generators';
+import { UnAuthorized, BadRequest } from '@plugins/errors';
 
 // Types
 import type { RequestHandler } from 'express';
-import type { IPolicy, IUserDoc } from '@models/types';
-import { UnAuthorized, BadRequest } from '@plugins/errors';
+import type {
+  IPolicy,
+  IPolicyDoc,
+  IPolicyLeanDoc,
+  IUserDoc,
+} from '@models/types';
+import type { IEditDatabaseResponse } from '@plugins/server/types';
 
 // Router
 const router = express.Router();
@@ -32,40 +39,84 @@ router.post('/update', (async (req, res) => {
     if (req.user) {
       const user: IUserDoc = req.user;
       const { policiesToUpdate }: { policiesToUpdate: IPolicy[] } = req.body;
-      const searchQs = {
-        $or: policiesToUpdate.map((policy) => {
-          if (policy._id) {
-            return {
-              _id: policy._id,
-            };
-          } else {
-            throw new BadRequest(
-              'policiesToUpdate',
-              'Requests.data to be of type IPolicy',
-            );
-          }
-        }),
+      const responseData: IEditDatabaseResponse = {
+        recordsUpdated: [],
+        totalRecordsUpdated: 0,
+        recordsNotUpdated: [],
+        totalRecordsNotUpdated: 0,
       };
-      const policyDocs = await Policies.find(searchQs).exec();
-      const promises = policiesToUpdate.map((modPolicy) => {
-        const [currentPolicyDoc] = policyDocs.filter(
-          (policy) => String(policy._id) === String(modPolicy._id),
+      const ids = policiesToUpdate.map((policy) => {
+        if (policy._id) {
+          return policy._id;
+        } else {
+          throw new BadRequest(
+            'policiesToUpdate',
+            'Requests.data.policiesToUpdate to be of type Partial<IPolicy>[] with mandatory property _id',
+          );
+        }
+      });
+      const getPolicyPromises = ids.map((id) =>
+        Policies.findOne({ _id: String(id) })
+          .lean()
+          .then((doc) => doc)
+          .catch(() => {
+            responseData.recordsNotUpdated.push({
+              id: String(id),
+              updated: false,
+            });
+            responseData.totalRecordsNotUpdated += 1;
+          }),
+      );
+      const allPromisePolicyDocs = await Promise.allSettled(getPolicyPromises);
+      const policyDocs: IPolicyLeanDoc[] = [];
+      allPromisePolicyDocs.forEach((promise) => {
+        if (promise.status === 'fulfilled') {
+          if (promise.value && promise.value !== undefined) {
+            policyDocs.push(promise.value);
+          }
+        }
+      });
+      const promises = policyDocs.map((currentPolicyDoc) => {
+        const [modPolicy] = policiesToUpdate.filter(
+          (policy) => String(policy._id) === String(currentPolicyDoc._id),
         );
         return policyAuth.edit(user, currentPolicyDoc, modPolicy);
       });
-      await Promise.all(promises);
-      okResponse(res, {
-        updated: true,
-        records: policiesToUpdate.map((policy) => String(policy._id)),
+      const promiseStatus = await Promise.allSettled(promises);
+      promiseStatus.forEach((promise) => {
+        if (promise.status === 'fulfilled') {
+          if (promise.value.updated) {
+            responseData.recordsUpdated.push(promise.value);
+            responseData.totalRecordsUpdated += 1;
+          } else {
+            responseData.recordsNotUpdated.push(promise.value);
+            responseData.totalRecordsNotUpdated += 1;
+          }
+        }
       });
+      okResponse(res, responseData);
     } else {
-      throw new UnAuthorized(
-        'User Not found in the Request, hence Unauthorized',
-      );
+      throw new UnAuthorized('User Not found in the Request - Unauthorized');
     }
   } catch (e) {
     errorResponseHandler(res, e);
   }
 }) as RequestHandler);
+
+router.post(
+  '/sample',
+  (async (req, res) =>
+    await editDatabaseExpressHandler<IPolicy, IPolicyDoc, IPolicyLeanDoc>(
+      req,
+      res,
+      Policies,
+      true,
+      {
+        bodyProp: 'policiesToUpdate',
+        modelName: 'Policy',
+      },
+      policyAuth.edit,
+    )) as RequestHandler,
+);
 
 export default router;
