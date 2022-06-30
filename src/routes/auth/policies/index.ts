7e93f005368fbc4a7b570c7638657f4798ccc491
tks18 <tksudharshan@gmail.com>
2022-06-30 08:20:58 +0530

    refactor(routes/auth): update policy, role files to use the refactored database handler

--- src/routes/auth/policies/index.ts
+++ src/routes/auth/policies/index.ts
@@ -9,21 +9,21 @@ import { policies as policyAuth } from '@plugins/auth';
 
 // Response Handlers
 import { errorResponseHandler, okResponse } from '@plugins/server/responses';
-import { editDatabaseExpressHandler } from '@plugins/server/generators';
-import { UnAuthorized, BadRequest } from '@plugins/errors';
+import { ExpressDatabaseHandler } from '@plugins/server/generators';
 
 // Types
 import type { RequestHandler } from 'express';
-import type {
-  IPolicy,
-  IPolicyDoc,
-  IPolicyLeanDoc,
-  IUserDoc,
-} from '@models/types';
-import type { IEditDatabaseResponse } from '@plugins/server/types';
+import type { IPolicy, IPolicyDoc, IPolicyLeanDoc } from '@models/types';
 
 // Router
 const router = express.Router();
+
+// Express Handlers
+const expressPolicyDatabaseHandler = new ExpressDatabaseHandler<
+  IPolicy,
+  IPolicyDoc,
+  IPolicyLeanDoc
+>(Policies, true);
 
 router.post('/list', (async (req, res) => {
   try {
@@ -34,83 +34,12 @@ router.post('/list', (async (req, res) => {
   }
 }) as RequestHandler);
 
-router.post('/update', (async (req, res) => {
-  try {
-    if (req.user) {
-      const user: IUserDoc = req.user;
-      const { policiesToUpdate }: { policiesToUpdate: IPolicy[] } = req.body;
-      const responseData: IEditDatabaseResponse = {
-        recordsUpdated: [],
-        totalRecordsUpdated: 0,
-        recordsNotUpdated: [],
-        totalRecordsNotUpdated: 0,
-      };
-      const ids = policiesToUpdate.map((policy) => {
-        if (policy._id) {
-          return policy._id;
-        } else {
-          throw new BadRequest(
-            'policiesToUpdate',
-            'Requests.data.policiesToUpdate to be of type Partial<IPolicy>[] with mandatory property _id',
-          );
-        }
-      });
-      const getPolicyPromises = ids.map((id) =>
-        Policies.findOne({ _id: String(id) })
-          .lean()
-          .then((doc) => doc)
-          .catch(() => {
-            responseData.recordsNotUpdated.push({
-              id: String(id),
-              updated: false,
-            });
-            responseData.totalRecordsNotUpdated += 1;
-          }),
-      );
-      const allPromisePolicyDocs = await Promise.allSettled(getPolicyPromises);
-      const policyDocs: IPolicyLeanDoc[] = [];
-      allPromisePolicyDocs.forEach((promise) => {
-        if (promise.status === 'fulfilled') {
-          if (promise.value && promise.value !== undefined) {
-            policyDocs.push(promise.value);
-          }
-        }
-      });
-      const promises = policyDocs.map((currentPolicyDoc) => {
-        const [modPolicy] = policiesToUpdate.filter(
-          (policy) => String(policy._id) === String(currentPolicyDoc._id),
-        );
-        return policyAuth.edit(user, currentPolicyDoc, modPolicy);
-      });
-      const promiseStatus = await Promise.allSettled(promises);
-      promiseStatus.forEach((promise) => {
-        if (promise.status === 'fulfilled') {
-          if (promise.value.updated) {
-            responseData.recordsUpdated.push(promise.value);
-            responseData.totalRecordsUpdated += 1;
-          } else {
-            responseData.recordsNotUpdated.push(promise.value);
-            responseData.totalRecordsNotUpdated += 1;
-          }
-        }
-      });
-      okResponse(res, responseData);
-    } else {
-      throw new UnAuthorized('User Not found in the Request - Unauthorized');
-    }
-  } catch (e) {
-    errorResponseHandler(res, e);
-  }
-}) as RequestHandler);
-
 router.post(
-  '/sample',
+  '/update',
   (async (req, res) =>
-    await editDatabaseExpressHandler<IPolicy, IPolicyDoc, IPolicyLeanDoc>(
+    await expressPolicyDatabaseHandler.editDatabaseHandler(
       req,
       res,
-      Policies,
-      true,
       {
         bodyProp: 'policiesToUpdate',
         modelName: 'Policy',
--- src/routes/auth/roles/index.ts
+++ src/routes/auth/roles/index.ts
@@ -9,15 +9,21 @@ import { roles as rolesAuth } from '@plugins/auth';
 
 // Response Handlers
 import { okResponse, errorResponseHandler } from '@plugins/server/responses';
-import { UnAuthorized, BadRequest } from '@plugins/errors';
 
 // Types
 import type { RequestHandler } from 'express';
-import type { IRole, IRoleDoc, IRoleLeanDoc, IUserDoc } from '@models/types';
-import { editDatabaseExpressHandler } from '@plugins/server/generators';
+import type { IRole, IRoleDoc, IRoleLeanDoc } from '@models/types';
+import { ExpressDatabaseHandler } from '@plugins/server/generators';
 
 // Router
 const router = express.Router();
+
+// Express Handlers
+const expressRoleDatabaseHandler = new ExpressDatabaseHandler<
+  IRole,
+  IRoleDoc,
+  IRoleLeanDoc
+>(Roles, true);
 
 router.post('/list', (async (req, res) => {
   try {
@@ -28,41 +34,12 @@ router.post('/list', (async (req, res) => {
   }
 }) as RequestHandler);
 
-router.post('/add', (async (req, res) => {
-  try {
-    if (req.user) {
-      const user: IUserDoc = req.user;
-      const { rolesToAdd }: { rolesToAdd: IRole[] } = req.body;
-      if (rolesToAdd) {
-        const promises = rolesToAdd.map((roles) => rolesAuth.add(user, roles));
-        const roleDocsAdded = await Promise.all(promises);
-        okResponse(res, {
-          model: 'Roles',
-          recordsAdded: roleDocsAdded,
-          totalRecordsAdded: roleDocsAdded.length,
-        });
-      } else {
-        throw new BadRequest(
-          'rolesToAdd',
-          'Request.body.rolesToAdd to be of type IRole[]',
-        );
-      }
-    } else {
-      throw new UnAuthorized('User Not found in the Request - Unauthorized');
-    }
-  } catch (e) {
-    errorResponseHandler(res, e);
-  }
-}) as RequestHandler);
-
 router.post(
   '/update',
   (async (req, res) =>
-    await editDatabaseExpressHandler<IRole, IRoleDoc, IRoleLeanDoc>(
+    await expressRoleDatabaseHandler.editDatabaseHandler(
       req,
       res,
-      Roles,
-      true,
       {
         bodyProp: 'rolesToUpdate',
         modelName: 'Role',
